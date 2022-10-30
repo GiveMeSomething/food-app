4411cf39ce9b2030165d5ee028af18b0409acb47
ADMIN\Administrator <longnvhn41@gmail.com>
2022-10-31 01:12:01 +0700

    feature: add fragment show chip with enum

---
+++ app/src/main/java/com/example/foodapp/ui/ChipFragment.java
@@ -0,0 +1,36 @@
+package com.example.foodapp.ui;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.foodapp.R;
+import com.google.android.material.chip.Chip;
+
+
+public class ChipFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_chip, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+}
